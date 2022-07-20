e376426c70adfb84a09ece65d65e02a0f0f13be1
TasukuY <alohaty81@gmail.com>
2022-07-20 15:14:40 -0600

    Initial Commit

---
+++ WeakvsStrong/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeakvsStrong
+//
+//  Created by Tasuku Yamamoto on 7/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
